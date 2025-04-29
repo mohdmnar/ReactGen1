ba2be76ac7f9202f1624929148d510d10d337273
mohdmnar <mohdmnar@amazon.com>
2025-04-30 03:08:39 +0530

    feaure commit2

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Muneer on the Vite and React logos to learn more
+        Muneer on the Vite and React logos to learn {variable}
       </p>
     </>
   )
